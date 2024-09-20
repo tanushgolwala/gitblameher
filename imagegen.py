aee7d8f6ae1a47f70e372638336573fe73bebdcf
Tanush Golwala <tanushgolwala@gmail.com>
2024-09-20 11:17:54 +0530

    image gen

--- imagegen.py
+++ imagegen.py
@@ -1,1 +1,10 @@
 import pollinations.ai as ai
+
+# Version 1
+model: ai.Image = ai.Image()
+image: ai.ImageObject = model.generate(
+      prompt="A cat playing with a ball",
+      # negative...width...height...height...seed...model...nologo
+)
+image.save("cat_playing_with_ball.png")
+print(image)
