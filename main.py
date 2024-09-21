c3c46875baa5894b84bb102ee97da1ea834da843
Ansh Mehta <anshkapilmehta@gmail.com>
2024-09-21 23:53:57 +0530

    frontend backend link

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from helpers import imagegen, musicgen
 from helpers.inputter import Inputter
 from typing import Annotated
@@ -7,6 +8,15 @@ from helpers.story_processor import *
 from utils.clearoutputfolders import copy_to_frontend
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"],  # Add your frontend URL here
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
@@ -42,6 +52,7 @@ def create_story_from_file(file: UploadFile):
     inputter = Inputter(file.file, get_flag_based_on_extension(file.filename))
     extracted_text = inputter.read()
     cleaned_text = inputter.clean_text(extracted_text)
+    print(cleaned_text) 
     numScenes = story_to_images(cleaned_text)
     copy_to_frontend()
 
